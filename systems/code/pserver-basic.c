6de522a24e1e959a894798bf68f575e4e08d9db8
Samuel Zhang <samuelzhang3@gmail.com>
2015-12-17 14:05:34 -0500

    basic client/server connection code

--- systems/code/pserver-basic.c
+++ systems/code/pserver-basic.c
@@ -11,17 +11,17 @@ int server_handshake( int *from_client ) {
   int to_client;
   char buffer[100];
 
-  mkfifo( "mario", 0644 ); //make pipe
-  *from_client = open( "mario", O_RDONLY ); //wait for a connection
+  mkfifo( "mario", 0644 ); //make pipe (WKP)
+  *from_client = open( "mario", O_RDONLY ); //open and wait for a connection
 
-  remove( "mario" );
-  read( *from_client, buffer, sizeof(buffer));
+  remove( "mario" ); //once connnected, remove pipe file
+  read( *from_client, buffer, sizeof(buffer)); //read from client
 
-  to_client = open( buffer, O_WRONLY );
+  to_client = open( buffer, O_WRONLY ); //connect downstream
 
-  strncpy( buffer, "its-a-me, mario!", sizeof(buffer) );
+  strncpy( buffer, "its-a-me, mario!", sizeof(buffer) ); 
 
-  write( to_client, buffer, sizeof(buffer));
+  write( to_client, buffer, sizeof(buffer)); //send initial message
 
   return to_client;
 }
@@ -36,6 +36,8 @@ int main() {
   while( 1 ) {
     read( from_client, buffer, sizeof(buffer));
     /* do stuff to buffer */
+    printf( "<server> received [%s]\n", buffer );
+    strncat( buffer, "purple monkey dishwasher", sizeof(buffer) - 1 ) ;
     write( to_client, buffer, sizeof(buffer));
   }
   
